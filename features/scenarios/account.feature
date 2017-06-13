4028e4756abe1177b6806c9907a75c6b556e3299
Ryan Devlin <ryandevlin86@gmail.com>
2017-06-12 23:04:27 -0400

    Adds features for account creation

---
+++ features/scenarios/account.feature
@@ -0,0 +1,33 @@
+@accounts
+Feature: Account
+
+  Scenario: Reaching the sign up page
+    Given a user reaches the homepage
+    When a user clicks Sign Up
+    Then the user should reach the sign up page
+
+  Scenario: Creating a new user
+    Given a user reaches the sign up page
+    When the user enters a valid username and password
+    Then a new user should be created
+    And the user should be redirected to the homepage and logged in
+
+  Scenario: User already exists
+    Given a user reaches the sign up page
+    When the user tries to create an existing account
+    Then an error message should be displayed
+
+  Scenario: Reaching the log in page
+    Given a user reaches the homepage
+    When a user clicks Log In
+    Then the user should reach the log in page
+
+  Scenario: Valid Log In
+    Given a user reaches the log in page
+    When the user enters a valid username and password
+    Then the user should be redirected to the homepage and logged in
+
+  Scenario: Invalid Log In
+    Given a user reaches the log in page
+    When the user enters a invalid username and password
+    Then an error message should be displayed
---
+++ features/step_definitions/setup_steps.rb
@@ -0,0 +1,52 @@
+Given(/^a user reaches the homepage$/) do
+    visit root_path
+end
+
+When(/^a user clicks Sign Up$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the user should reach the sign up page$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a user reaches the sign up page$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the user enters a valid username and password$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^a new user should be created$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the user should be redirected to the homepage and logged in$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the user tries to create an existing account$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^an error message should be displayed$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^a user clicks Log In$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the user should reach the log in page$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a user reaches the log in page$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the user enters a invalid username and password$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
