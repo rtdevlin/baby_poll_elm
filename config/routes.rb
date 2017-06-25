1a99e43b0cb3437a41dd653b63bdc2df3926ff63
Ryan Devlin <ryandevlin86@gmail.com>
2017-06-24 20:04:40 -0400

    create controller and view for poll

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
 
+  get '/poll/:id', to: 'poll#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
